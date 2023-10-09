c8fdd690587226097f6ef30cbc6cfcf6c8133504
inno <kanghao1@mediav.cn>
2023-10-09 14:51:41 +0800

    六第五次修改

--- src/Six.tsx
+++ src/Six.tsx
@@ -5,8 +5,8 @@ export const Six = () => {
     <div>
         <h2>1</h2>
         <h2>2</h2>
-        <h2>3</h2>
-        <h2>4</h2>
+       <div>3</div>
+       <div>4</div>
         finish
     </div>
   )
